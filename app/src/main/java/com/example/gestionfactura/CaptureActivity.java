14681c0d60775d5e4c15021aeb0674846388ba40
Bryan-Edward <edwardbrian96@gmail.com>
2019-05-31 14:19:51 -0500

    camera

--- app/src/main/java/com/example/gestionfactura/CaptureActivity.java
+++ app/src/main/java/com/example/gestionfactura/CaptureActivity.java
@@ -1,9 +1,14 @@
 package com.example.gestionfactura;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
@@ -14,15 +19,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CaptureActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final int REQUEST_PERMISSION_STORAGE_SAVE = 101;
-    private final int REQUEST_PERMISSION_STORAGE_DELETE = 101;
+    private final int REQUEST_PERMISSION_STORAGE_DELETE = 100;
 
 
     //valor constante para la camara
@@ -69,8 +77,6 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
     }
 
 
-
-
     private boolean hasPermissionToWrite(){
         //Para los permisos si deja escribir en la memoria externa
         return ContextCompat.checkSelfPermission(
@@ -90,7 +96,7 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
             //onDeleteMenuTap();
             return true;
         }else if(item.getItemId() == R.id.save){
-            //onSaveMenuTap();
+            onSaveMenuTap();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -99,6 +105,88 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
+        if(v == buttonCamera){
+            dispatchTakePictureIntent();
+        }
+    }
 
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(bitmap);
+            txtMessage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+   public void onSaveMenuTap(){
+       if(!hasPermissionToWrite()){
+           ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_PERMISSION_STORAGE_SAVE);
+       }else{
+           if(imageView.getDrawable() !=null){
+               createFolder();
+               String stogareDir = Environment.getExternalStorageDirectory() + "/UOCImageApp";
+
+               createImageFile(stogareDir, this.File_NAME, bitmap);
+           }else{
+               Toast.makeText(this, "tome unafoto primero", Toast.LENGTH_SHORT).show();
+           }
+       }
+   }
+
+    private void createFolder() {
+       String myFolder = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+FOLDER_NAME;
+       File folder = new File(myFolder);
+       if(!folder.exists()){
+           if(!folder.mkdir()){
+               Toast.makeText(this, "no se pude crear", Toast.LENGTH_SHORT).show();
+           }else{
+               Toast.makeText(this, "se puedp crear", Toast.LENGTH_SHORT).show();
+           }
+       }
+    }
+
+    private void createImageFile(String stogaDir,String fileName, Bitmap bitmap){
+        try{
+            File myFile = new File(stogaDir,fileName);
+            FileOutputStream stream = new FileOutputStream(myFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            stream.flush();
+            stream.close();
+            Toast.makeText(this, "iMAGEN GUARDADA", Toast.LENGTH_SHORT).show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_PERMISSION_STORAGE_DELETE:{
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                }else {
+                    Toast.makeText(this, "permiso revocado", Toast.LENGTH_SHORT).show();
+                }
+            }
+            case REQUEST_PERMISSION_STORAGE_SAVE:{
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    createFolder();
+                    String stoageDir = Environment.getExternalStorageDirectory()+"/UOCImageApp";
+                    createImageFile(stoageDir,File_NAME,bitmap);
+                }else{
+                    Toast.makeText(this, "permiso de guarda denegado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
